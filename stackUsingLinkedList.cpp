17a89d0cb9dcf5a7c770214a55be5c09dddaade1
khansa-qonita <khansaqonita420@gmail.com>
2026-05-14 10:35:29 +0700

    implementasi logika push pada stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -26,7 +26,12 @@ public:
     }
     int push(int value)
     {
-        
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value :" << value << endl;
+        return value;
     }
 }
 
